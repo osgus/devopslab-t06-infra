427574c82bd904ad9ad5e3a5b7671f3c720577f5
osgus <44347610+osgus@users.noreply.github.com>
2022-08-29 23:28:44 -0300

    fix credentials

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/gustavo_2102079/gcpkey.json")
-
   project = "gold-gateway-360700"
   region  = "us-west4"
   zone    = "us-west4-b"
